ac68f02db0408252c429eec7d3d54fac869b2eac
Angela <115098835+AngelaMCeroula@users.noreply.github.com>
2023-02-25 14:46:56 +0000

    move and look controls

---
+++ Assets/Scripts/PlayerScripts/CameraLook.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float lookSensitivity = 100f;
+    public Transform playerBody;
+    public float xRotation = 0f;
+    public float cameraX;
+    public float cameraY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //cameraX = cameraX * lookSensitivity * Time.deltaTime;
+        //cameraY = cameraY * lookSensitivity * Time.deltaTime;
+
+        xRotation -= cameraY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f,0f);
+        playerBody.Rotate(Vector3.up * cameraX);
+    }
+}
--- Assets/Scripts/PlayerScripts/PlayerVoiceController.cs
+++ Assets/Scripts/PlayerScripts/PlayerVoiceController.cs
@@ -28,13 +28,31 @@ public class PlayerVoiceController : MonoBehaviour
     // voice
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    
+    //Camerascript
+    public CameraLook cameraLook;
 
 
     private void Start()
     {
+        //movement and camera controls
         actions.Add("forward", MoveForward);
         actions.Add("move", MoveForward);
+        actions.Add("walk", MoveForward);
         actions.Add("stop", Stop);
+        actions.Add("look up", LookUp);
+        actions.Add("up", LookUp);
+        actions.Add("look down", LookDown);
+        actions.Add("down", LookDown);
+        actions.Add("look right", LookRight);
+        actions.Add("right", LookRight);
+        actions.Add("look left", LookLeft);
+        actions.Add("left", LookLeft);
+        actions.Add("turn around", TurnAround);
+        actions.Add("turn", TurnAround);
+        actions.Add("look back", TurnAround);
+        actions.Add("look front", LookFront);
+        actions.Add("front", LookFront);
 
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
@@ -73,16 +91,32 @@ public class PlayerVoiceController : MonoBehaviour
         controller.Move(move * (speed * Time.deltaTime));
 
         //jump input
+        /*
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jump * -2f * gravity);
         }
+        */
         
         // gravity 
         velocity.y += gravity * Time.deltaTime;
         
         //movement falling motion speed
         controller.Move(velocity * Time.deltaTime);
+    }
+    
+    private void Talk()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            keywordRecognizer.Start();
+            Debug.Log(keywordRecognizer.IsRunning.ToString());
+        }
+
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            keywordRecognizer.Stop();
+        }
     }
 
     private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
@@ -101,21 +135,45 @@ public class PlayerVoiceController : MonoBehaviour
     {
         x = 0;
         z = 0;
+        cameraLook.cameraX = 0f;
+        cameraLook.cameraY = 0f;
 
     }
 
-    private void Talk()
+    private void TurnAround()
     {
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            keywordRecognizer.Start();
-            Debug.Log(keywordRecognizer.IsRunning.ToString());
-        }
+        
+        transform.localRotation = Quaternion.Euler(0f,180f,0f);
 
-        if (Input.GetKeyUp(KeyCode.V))
-        {
-            keywordRecognizer.Stop();
-        }
     }
-    
+
+    private void LookUp()
+    {
+        cameraLook.cameraY += 0.2f;
+        
+    }
+
+
+    private void LookDown()
+    {
+        cameraLook.cameraY -= 0.2f;
+        
+    }
+
+    private void LookRight()
+    {
+        cameraLook.cameraX += 0.2f;
+    }
+
+    private void LookLeft()
+    {
+        cameraLook.cameraX -= 0.2f;
+    }
+
+    private void LookFront()
+    {
+        cameraLook.xRotation = 0;
+
+
+    }
 }
