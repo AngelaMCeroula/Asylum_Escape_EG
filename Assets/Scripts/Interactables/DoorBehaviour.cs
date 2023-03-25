3f7d894a70fa35777ac681aa229b66fdd78f2664
Angela <115098835+AngelaMCeroula@users.noreply.github.com>
2023-03-25 20:43:29 +0000

    It works_1

--- Assets/Scripts/Interactables/DoorBehaviour.cs
+++ Assets/Scripts/Interactables/DoorBehaviour.cs
@@ -10,13 +10,15 @@ public class DoorBehaviour : MonoBehaviour
     private PlayerVoiceController PVC;
     [SerializeField] private string neededItemName;
     private bool doorClosed = true;
+    private GameObject doorExitTriggerArea;
     
 
     void Start()
     {
         inventorySystem = GameObject.Find("Player").GetComponent<InventorySystem>();
         PVC = GameObject.Find("Player").GetComponent<PlayerVoiceController>();
-
+        doorExitTriggerArea = GameObject.Find("DoorExitTrigger");
+        doorExitTriggerArea.SetActive(false);
     }
 
     public void SearchDoor()
@@ -37,6 +39,7 @@ public class DoorBehaviour : MonoBehaviour
         {
             doorClosed = false;
             PVC._canLeave = true;
+            doorExitTriggerArea.SetActive(true);
             //levelManager.End2_Escape();
             Destroy(gameObject);
         }
--- Assets/Scripts/Interactables/WindowBehaviour.cs
+++ Assets/Scripts/Interactables/WindowBehaviour.cs
@@ -10,12 +10,16 @@ public class WindowBehaviour : MonoBehaviour
     private InventorySystem inventorySystem;
     private PlayerVoiceController PVC;
     [SerializeField] private string neededItemName;
+    [SerializeField] private string neededItemName2;
     private bool windowIntact = false;
+    private GameObject windowExitTriggerArea;
 
     void Start()
     {
         inventorySystem = GameObject.Find("Player").GetComponent<InventorySystem>();
         PVC = GameObject.Find("Player").GetComponent<PlayerVoiceController>();
+        windowExitTriggerArea = GameObject.Find("WindowExitTrigger");
+        windowExitTriggerArea.SetActive(false);
     }
     public void SearchWindow()
     {
@@ -31,10 +35,11 @@ public class WindowBehaviour : MonoBehaviour
 
     public void BreakGlass()
     {
-        if (inventorySystem.HasItem(neededItemName) && windowIntact == true)
+        if ((inventorySystem.HasItem(neededItemName) || inventorySystem.HasItem(neededItemName2)) && windowIntact == true)
         {
             windowIntact = false;
             PVC._canLeave = true;
+            windowExitTriggerArea.SetActive(true);
             //levelManager.End2_Escape();
             Destroy(gameObject);
         }
