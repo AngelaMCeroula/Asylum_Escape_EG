1a15b98717decc040bbe8314222b12c54aa8ee62
Angela <115098835+AngelaMCeroula@users.noreply.github.com>
2023-03-28 15:29:05 +0100

    intro fades

--- Assets/Scripts/UIScripts/IntroManager.cs
+++ Assets/Scripts/UIScripts/IntroManager.cs
@@ -6,37 +6,51 @@ using UnityEngine.SceneManagement;
 public class IntroManager : MonoBehaviour
 {
     [SerializeField]private GameObject intro1, intro2, intro3;
-    [SerializeField]private CanvasGroup canvas1, canvas2, canvas3;
-    [SerializeField]private float timeInSeconds = 5;
-    [SerializeField]private float timeInBetweenFade = 2;
+    [SerializeField]private Animator transition;
+    
+    [SerializeField]private float time1 = 3;
+    [SerializeField]private float time2 = 5;
+    [SerializeField]private float time3 = 5;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        canvas1 = intro1.GetComponent<CanvasGroup>();
-        canvas2 = intro2.GetComponent<CanvasGroup>();
-        canvas3 = intro3.GetComponent<CanvasGroup>();
+        transition = GameObject.Find("CrossFade").GetComponent<Animator>();
+        intro1.SetActive(true);
+        intro2.SetActive(false);
+        intro3.SetActive(false);
+        FadeIn();
+    }
+
+    void FadeIn()
+    {
+        StartCoroutine(Fade1());
+    }
+    
+
+    IEnumerator Fade1()
+    {
+        transition.SetTrigger("FULL");
+        yield return new WaitForSeconds(time1);
+        intro1.SetActive(false);
+        intro2.SetActive(true);
+        StartCoroutine(Next1());
+    }
+
+    IEnumerator Next1()
+    {
+        yield return new WaitForSeconds(time2);
+        intro2.SetActive(false);
+        intro3.SetActive(true);
+        StartCoroutine(Next2());
         
-        StartCoroutine(Delay1());
     }
-    
-    
-
-    IEnumerator Delay1()
+    IEnumerator Next2()
     {
-        yield return new WaitForSeconds(timeInSeconds);
-        canvas1.alpha -= Time.deltaTime;
-        StartCoroutine(Delay2());
-    }
-    IEnumerator Delay2()
-    {
-        yield return new WaitForSeconds(timeInSeconds);
-        canvas2.alpha += Time.deltaTime;
-        //SceneManager.LoadScene("MainMenu");
-
+        yield return new WaitForSeconds(time3);
+        SceneManager.LoadScene("MainMenu");
 
     }
-    
-    
+   
 }
